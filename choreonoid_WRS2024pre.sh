6a93c7fb92c43a35079121d1621001fcad3336c3
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2024-10-19 00:32:40 +0900

    Add files via upload

---
+++ choreonoid_WRS2024pre.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd
+sudo apt install git
+git clone https://github.com/choreonoid/choreonoid.git
+git clone https://github.com/wrs-frei-simulation/WRS-Pre-2024.git choreonoid/ext/WRS2024PRE
+git clone https://github.com/k38-suzuki/hairo-world-plugin.git choreonoid/ext/hairo-world-plugin
+git pull
+cd choreonoid
+misc/script/install-requisites-ubuntu-22.04.sh
+mkdir build
+cd build
+cmake .. -DBUILD_WRS2018=ON -DBUILD_SCENE_EFFECTS_PLUGIN=ON -DBUILD_HAIRO_WORLD_PLUGIN=ON -DENABLE_INSTALL_RPATH_USE_LINK_PATH=ON
+make
+sudo make install
