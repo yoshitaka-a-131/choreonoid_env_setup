13a70b6e32b6b818c636205382998fd1e1e1e83b
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2024-10-23 20:14:03 +0900

    Create AGX.sh

---
+++ AGX.sh
@@ -0,0 +1,7 @@
+cd
+cd ダウンロード
+dpkg -i agx-2.38.0.0-amd64-ubuntu_22.04.deb
+sudo apt install gdebi-core
+sudo gdebi agx-2.38.0.0-amd64-ubuntu_22.04.deb
+cd
+echo source /opt/Algoryx/AGX-2.38.0.0/setup_env.bash >> .bashrc
