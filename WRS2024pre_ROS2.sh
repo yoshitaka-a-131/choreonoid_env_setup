007357fa6ff95ce5dcea6d59f2edb57a01d41005
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2024-10-23 19:46:16 +0900

    Update WRS2024pre_ROS2.sh

--- WRS2024pre_ROS2.sh
+++ WRS2024pre_ROS2.sh
@@ -1,1 +1,32 @@
-a
+cd
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update
+sudo apt upgrade
+sudo apt install ros-humble-desktop
+sudo apt install ros-humble-compressed-image-transport
+sudo apt install python3-colcon-common-extensions
+
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+cd
+sudo apt install git
+mkdir -p ~/ros2_ws/src
+cd ~/ros2_ws/src
+git clone https://github.com/choreonoid/choreonoid.git
+git clone https://github.com/choreonoid/choreonoid_ros.git
+git clone https://github.com/choreonoid/choreonoid_ros2_mobile_robot_tutorial.git
+git clone https://github.com/wrs-frei-simulation/WRS-Pre-2024.git choreonoid/ext/WRS2024PRE
+git clone https://github.com/k38-suzuki/hairo-world-plugin.git choreonoid/ext/hairo-world-plugin
+git clone https://github.com/k38-suzuki/choreonoid_ros2_sample_drone_tutorial.git
+git pull
+cd choreonoid
+misc/script/install-requisites-ubuntu-22.04.sh
+cd
+cd ~/ros2_ws
+colcon build --symlink-install --cmake-args -DBUILD_AGX_DYNAMICS_PLUGIN=ON -DBUILD_AGX_BODYEXTENSION_PLUGIN=ON -DBUILD_WRS2018=ON -DBUILD_SCENE_EFFECTS_PLUGIN=ON -DBUILD_HAIRO_WORLD_PLUGIN=ON -DENABLE_INSTALL_RPATH_USE_LINK_PATH=ON
