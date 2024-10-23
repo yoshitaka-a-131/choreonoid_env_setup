e3e7db9ed38005bc8d5395e233b5d80b6c787021
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2024-10-23 22:17:36 +0900

    Update AGX.sh

--- AGX.sh
+++ AGX.sh
@@ -4,5 +4,5 @@ sudo dpkg -i agx-2.38.0.0-amd64-ubuntu_22.04.deb
 sudo apt install gdebi-core
 sudo gdebi agx-2.38.0.0-amd64-ubuntu_22.04.deb
 cd
-echo source /opt/Algoryx/AGX-2.38.0.0/setup_env.bash >> .bashrc
+echo "source /opt/Algoryx/AGX-2.38.0.0/setup_env.bash" >> .bashrc
 export LD_LIBRARY_PATH=/opt/Algoryx/AGX-2.38.0.0/lib:$LD_LIBRARY_PATH
