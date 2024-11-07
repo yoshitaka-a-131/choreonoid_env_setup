a1d04f75b3d08258b0b13bd41c76bd5688f9d3da
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2024-11-07 18:13:24 +0900

    Update WRS2024pre_with_ROS2.sh

--- WRS2024pre_with_ROS2.sh
+++ WRS2024pre_with_ROS2.sh
@@ -8,9 +8,10 @@ git clone https://github.com/choreonoid/choreonoid_ros2_mobile_robot_tutorial.gi
 git clone https://github.com/wrs-frei-simulation/WRS-Pre-2024.git choreonoid/ext/WRS2024PRE
 git clone https://github.com/k38-suzuki/hairo-world-plugin.git choreonoid/ext/hairo-world-plugin
 git clone https://github.com/k38-suzuki/choreonoid_ros2_sample_drone_tutorial.git
-git clone https://github.com/yoshitaka-a-131/choreonoid_env_setup.git
 cp $HOME/choreonoid_env_setup/registration_preliminaries.yaml $HOME/ros2_ws/src/choreonoid/ext/WRS2024PRE/registration/registration_preliminaries.yaml
 cp $HOME/choreonoid_env_setup/registration_finals.yaml $HOME/ros2_ws/src/choreonoid/ext/WRS2024PRE/registration/registration_finals.yaml
+cp $HOME/choreonoid_env_setup/WRS2024pre_preliminaries_launch.xml $HOME/ros2_ws/src/choreonoid_ros/launch/ros2/WRS2024pre_preliminaries_launch.xml
+cp $HOME/choreonoid_env_setup/WRS2024pre_finals_launch.xml $HOME/ros2_ws/src/choreonoid_ros/launch/ros2/WRS2024pre_finals_launch.xml
 cd choreonoid
 misc/script/install-requisites-ubuntu-22.04.sh
 rosdep install -y --from-paths ~/ros2_ws/src --ignore-src
