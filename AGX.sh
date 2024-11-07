91b69434bb8c80e1f2e936344680b24fdb601554
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2024-11-07 18:03:18 +0900

    Update AGX.sh

--- AGX.sh
+++ AGX.sh
@@ -3,7 +3,6 @@ wget -P $HOME https://www.algoryx.se/download/installers/old/agx-2.38.0.0-amd64-
 sudo dpkg -i agx-2.38.0.0-amd64-ubuntu_22.04.deb
 sudo apt install gdebi-core
 sudo gdebi agx-2.38.0.0-amd64-ubuntu_22.04.deb
-cd
 echo "source /opt/Algoryx/AGX-2.38.0.0/setup_env.bash" >> .bashrc
 export LD_LIBRARY_PATH=/opt/Algoryx/AGX-2.38.0.0/lib:$LD_LIBRARY_PATH
 gnome-terminal -- bash "$HOME/choreonoid_env_setup/WRS2024pre_without_ROS2.sh"
