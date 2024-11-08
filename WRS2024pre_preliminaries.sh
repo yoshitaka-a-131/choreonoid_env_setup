0458222bfd100496d99b63b172cbb448befc0f82
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2024-11-09 00:50:10 +0900

    Update WRS2024pre_preliminaries.sh

--- WRS2024pre_preliminaries.sh
+++ WRS2024pre_preliminaries.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gnome-terminal -- bash -c "bash $HOME/choreonoid_env_setup/WRS2024pre_preliminaries.sh;bash"
+gnome-terminal -- bash -c "bash $HOME/choreonoid_env_setup/WRS2024pre_preliminaries2.sh;bash"
