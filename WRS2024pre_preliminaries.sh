188996b8b40635f749d3a8b1eb0a4fd84cf88fe8
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2024-11-09 00:43:50 +0900

    Update WRS2024pre_preliminaries.sh

--- WRS2024pre_preliminaries.sh
+++ WRS2024pre_preliminaries.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gnome-terminal -- bash -c "echo 'シェルスクリプト実効完了！';ros2 launch choreonoid_ros WRS2024pre_preliminaries.launch.xml; bash"
+gnome-terminal -- bash -c "echo 'シェルスクリプト実効完了！'"
 ros2 launch choreonoid_ros WRS2024pre_preliminaries.launch.xml
