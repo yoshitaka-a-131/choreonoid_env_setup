45083b91056f24df60292695c5cb3362104884c8
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2024-11-12 19:37:59 +0900

    Update Install_ROS2.sh

--- shell_script/Install_ROS2.sh
+++ shell_script/Install_ROS2.sh
@@ -1,14 +1,14 @@
 cd
-sudo apt install software-properties-common
+sudo apt install -y software-properties-common
 sudo add-apt-repository universe
 sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
 sudo apt upgrade
-sudo apt install ros-humble-desktop
-sudo apt install ros-humble-compressed-image-transport
-sudo apt install python3-colcon-common-extensions
+sudo apt install -y ros-humble-desktop
+sudo apt install -y ros-humble-compressed-image-transport
+sudo apt install -y python3-colcon-common-extensions
 echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 sudo apt install python3-colcon-common-extensions
