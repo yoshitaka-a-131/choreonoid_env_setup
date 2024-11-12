2e22862f05fe475fcac1f6bca57312d1e07e70ef
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2024-11-12 19:56:49 +0900

    Update WRS2024pre_with_ROS2.sh

--- shell_script/WRS2024pre_with_ROS2.sh
+++ shell_script/WRS2024pre_with_ROS2.sh
@@ -1,4 +1,5 @@
 cd
+sudo apt install -y python-rosdep
 mkdir -p ~/ros2_ws/src
 cd ros2_ws
 cd src
@@ -8,21 +9,32 @@ git clone https://github.com/choreonoid/choreonoid_ros2_mobile_robot_tutorial.gi
 git clone https://github.com/wrs-frei-simulation/WRS-Pre-2024.git choreonoid/ext/WRS2024PRE
 git clone https://github.com/k38-suzuki/hairo-world-plugin.git choreonoid/ext/hairo-world-plugin
 git clone https://github.com/k38-suzuki/choreonoid_ros2_sample_drone_tutorial.git
-cp $HOME/choreonoid_env_setup/registration_preliminaries.yaml $HOME/ros2_ws/src/choreonoid/ext/WRS2024PRE/registration/registration_preliminaries.yaml
-cp $HOME/choreonoid_env_setup/registration_finals.yaml $HOME/ros2_ws/src/choreonoid/ext/WRS2024PRE/registration/registration_finals.yaml
-cp $HOME/choreonoid_env_setup/WRS2024pre_preliminaries.launch.xml $HOME/ros2_ws/src/choreonoid_ros/launch/ros2/WRS2024pre_preliminaries.launch.xml
-cp $HOME/choreonoid_env_setup/WRS2024pre_finals.launch.xml $HOME/ros2_ws/src/choreonoid_ros/launch/ros2/WRS2024pre_finals.launch.xml
 cd choreonoid
 misc/script/install-requisites-ubuntu-22.04.sh
-rosdep install -y --from-paths ~/ros2_ws/src --ignore-src
+cd ~/ros2_ws/src
+ros2 pkg create WRS2024pre_ROS2
+cd WRS2024pre_ROS2
+mkdir launch
+cp $HOME/choreonoid_env_setup/launch/WRS2024pre_preliminaries.launch.xml $HOME/ros2_ws/src/WRS2024pre_ROS2/launch/WRS2024pre_preliminaries.launch.xml
+cp $HOME/choreonoid_env_setup/launch/WRS2024pre_finals.launch.xml $HOME/ros2_ws/src/WRS2024pre_ROS2/launch/WRS2024pre_finals.launch.xml
+cd ~/ros2_ws/src/WRS2024pre_ROS2
+sed -i '12a  <exec_depend>ros2launch</exec_depend>' package.xml
+sed -i '10a # install launch files ' CMakeLists.txt
+sed -i '11a install(' CMakeLists.txt 
+sed -i '12a DIRECTORY launch' CMakeLists.txt 
+sed -i '13a DESTINATION share/${PROJECT_NAME}/' CMakeLists.txt 
+sed -i '14a )' CMakeLists.txt 
 cd ~/ros2_ws
+rosdep install -y --from-paths ~/ros2_ws/src --ignore-src
+sudo rosdep init
+rosdep update
 colcon build --symlink-install --cmake-args -DBUILD_AGX_DYNAMICS_PLUGIN=ON -DBUILD_AGX_BODYEXTENSION_PLUGIN=ON -DBUILD_WRS2018=ON -DBUILD_SCENE_EFFECTS_PLUGIN=ON -DBUILD_HAIRO_WORLD_PLUGIN=ON -DENABLE_INSTALL_RPATH_USE_LINK_PATH=ON
 echo "source $HOME/ros2_ws/install/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-cp $HOME/choreonoid_env_setup/WRS2024pre_preliminaries.desktop $HOME/デスクトップ/WRS2024pre_preliminaries.desktop
-cp $HOME/choreonoid_env_setup/WRS2024pre_finals.desktop $HOME/デスクトップ/WRS2024pre_finals.desktop
-chmod 774 $HOME/choreonoid_env_setup/WRS2024pre_preliminaries.sh
-chmod 774 $HOME/choreonoid_env_setup/WRS2024pre_finals.sh
+cp $HOME/choreonoid_env_setup/desktop/WRS2024pre_preliminaries.desktop $HOME/デスクトップ/WRS2024pre_preliminaries.desktop
+cp $HOME/choreonoid_env_setup/desktop/WRS2024pre_finals.desktop $HOME/デスクトップ/WRS2024pre_finals.desktop
+chmod 774 $HOME/choreonoid_env_setup/desktop/WRS2024pre_preliminaries.sh
+chmod 774 $HOME/choreonoid_env_setup/desktop/WRS2024pre_finals.sh
 gio set $HOME/デスクトップ/WRS2024pre_preliminaries.desktop metadata::trusted true
 chmod 774 $HOME/デスクトップ/WRS2024pre_preliminaries.desktop
 gio set $HOME/デスクトップ/WRS2024pre_finals.desktop metadata::trusted true
