cb1136c0017231e7a1f87d489ed4256dbf79e1b5
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2024-11-12 19:38:23 +0900

    Update Install_AGX.sh

--- shell_script/Install_AGX.sh
+++ shell_script/Install_AGX.sh
@@ -1,7 +1,7 @@
 cd
 wget -P $HOME/choreonoid_env_setup/shell_script https://www.algoryx.se/download/installers/old/agx-2.38.0.0-amd64-ubuntu_22.04.deb
 sudo dpkg -i $HOME/choreonoid_env_setup/shell_script/agx-2.38.0.0-amd64-ubuntu_22.04.deb
-sudo apt -y install gdebi-core
+sudo apt install -y gdebi-core
 sudo gdebi $HOME/choreonoid_env_setup/shell_script/agx-2.38.0.0-amd64-ubuntu_22.04.deb
 echo "source /opt/Algoryx/AGX-2.38.0.0/setup_env.bash" >> .bashrc
 echo "export LD_LIBRARY_PATH=/opt/Algoryx/AGX-2.38.0.0/lib:$LD_LIBRARY_PATH" >> .bashrc
