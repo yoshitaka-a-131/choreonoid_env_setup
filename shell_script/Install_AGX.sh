4932ac0d0d11dfade818ad51010f06326fe1395b
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2024-11-12 20:38:45 +0900

    Update Install_AGX.sh

--- shell_script/Install_AGX.sh
+++ shell_script/Install_AGX.sh
@@ -6,4 +6,4 @@ sudo gdebi $HOME/choreonoid_env_setup/shell_script/agx-2.38.0.0-amd64-ubuntu_22.
 echo "source /opt/Algoryx/AGX-2.38.0.0/setup_env.bash" >> .bashrc
 echo "export LD_LIBRARY_PATH=/opt/Algoryx/AGX-2.38.0.0/lib:$LD_LIBRARY_PATH" >> .bashrc
 source ~/.bashrc
-
+python3 $HOME/choreonoid_env_setup/shell_script/USB.py
