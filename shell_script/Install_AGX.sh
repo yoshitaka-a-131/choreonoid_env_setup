f018816971ae45da451d32694a51ef00a334075a
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2024-11-12 17:40:40 +0900

    Update and rename AGX.sh to shell_script/Install_AGX.sh

--- shell_script/Install_AGX.sh
+++ shell_script/Install_AGX.sh
@@ -6,4 +6,4 @@ sudo gdebi agx-2.38.0.0-amd64-ubuntu_22.04.deb
 echo "source /opt/Algoryx/AGX-2.38.0.0/setup_env.bash" >> .bashrc
 echo "export LD_LIBRARY_PATH=/opt/Algoryx/AGX-2.38.0.0/lib:$LD_LIBRARY_PATH" >> .bashrc
 source ~/.bashrc
-gnome-terminal -- bash -c "bash $HOME/choreonoid_env_setup/ROS2.sh;bash"
+
