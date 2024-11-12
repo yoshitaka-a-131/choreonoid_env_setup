e05cfe7dfc0a01e48ac63737b76e913009c461ba
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2024-11-12 18:39:00 +0900

    Update Install_AGX.sh

--- shell_script/Install_AGX.sh
+++ shell_script/Install_AGX.sh
@@ -1,8 +1,8 @@
 cd
-wget -P $HOME https://www.algoryx.se/download/installers/old/agx-2.38.0.0-amd64-ubuntu_22.04.deb
-sudo dpkg -i agx-2.38.0.0-amd64-ubuntu_22.04.deb
+wget -P $HOME/choreonoid_env_setup/shell_script https://www.algoryx.se/download/installers/old/agx-2.38.0.0-amd64-ubuntu_22.04.deb
+sudo dpkg -i $HOME/choreonoid_env_setup/shell_script/agx-2.38.0.0-amd64-ubuntu_22.04.deb
 sudo apt install gdebi-core
-sudo gdebi agx-2.38.0.0-amd64-ubuntu_22.04.deb
+sudo gdebi $HOME/choreonoid_env_setup/shell_script/agx-2.38.0.0-amd64-ubuntu_22.04.deb
 echo "source /opt/Algoryx/AGX-2.38.0.0/setup_env.bash" >> .bashrc
 echo "export LD_LIBRARY_PATH=/opt/Algoryx/AGX-2.38.0.0/lib:$LD_LIBRARY_PATH" >> .bashrc
 source ~/.bashrc
