00eceb13dc41154758e448ee144245fea823eb1d
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2024-11-13 21:48:14 +0900

    Update WRS2024pre_without_ROS2.sh

--- shell_script/WRS2024pre_without_ROS2.sh
+++ shell_script/WRS2024pre_without_ROS2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd
-sudo apt install -y git
 git clone https://github.com/choreonoid/choreonoid.git
 git clone https://github.com/wrs-frei-simulation/WRS-Pre-2024.git choreonoid/ext/WRS2024PRE
 git clone https://github.com/k38-suzuki/hairo-world-plugin.git choreonoid/ext/hairo-world-plugin
@@ -12,4 +11,3 @@ cd build
 cmake .. -DBUILD_AGX_DYNAMICS_PLUGIN=ON -DBUILD_AGX_BODYEXTENSION_PLUGIN=ON -DBUILD_WRS2018=ON -DBUILD_SCENE_EFFECTS_PLUGIN=ON -DBUILD_HAIRO_WORLD_PLUGIN=ON -DENABLE_INSTALL_RPATH_USE_LINK_PATH=ON
 make
 sudo make install
-
