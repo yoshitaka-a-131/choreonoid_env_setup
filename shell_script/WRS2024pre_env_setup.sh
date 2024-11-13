95cc24c9f189b6da10aeb25f8c5a6255769484af
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2024-11-13 21:44:37 +0900

    Update WRS2024pre_env_setup.sh

--- shell_script/WRS2024pre_env_setup.sh
+++ shell_script/WRS2024pre_env_setup.sh
@@ -2,25 +2,25 @@
 read line < select.txt
 
 if echo "$line" | grep -q "1"; then
-  gnome-terminal -- bash -c "bash $HOME/choreonoid_env_setup/shell_script/Install_AGX.sh;bash"
+  bash $HOME/choreonoid_env_setup/shell_script/Install_AGX.sh
 else
   :
 fi
 
 if echo "$line" | grep -q "2"; then
-  gnome-terminal -- bash -c "bash $HOME/choreonoid_env_setup/shell_script/Install_ROS2.sh;bash"
+  bash $HOME/choreonoid_env_setup/shell_script/Install_ROS2.sh
 else
   :
 fi
 
 if echo "$line" | grep -q "3"; then
-  gnome-terminal -- bash -c "bash $HOME/choreonoid_env_setup/shell_script/WRS2024pre_without_ROS2.sh;bash"
+  bash $HOME/choreonoid_env_setup/shell_script/WRS2024pre_without_ROS2.sh
 else
   :
 fi
 
 if echo "$line" | grep -q "4"; then
-  gnome-terminal -- bash -c "bash $HOME/choreonoid_env_setup/shell_script/WRS2024pre_with_ROS2.sh;bash"
+  bash $HOME/choreonoid_env_setup/shell_script/WRS2024pre_with_ROS2.sh
 else
   :
 fi
