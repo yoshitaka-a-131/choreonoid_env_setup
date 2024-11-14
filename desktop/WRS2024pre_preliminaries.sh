58692ae310d7368eb357c709efb7f2e2f0787d4d
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2024-11-14 21:37:45 +0900

    Update WRS2024pre_preliminaries.sh

--- desktop/WRS2024pre_preliminaries.sh
+++ desktop/WRS2024pre_preliminaries.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+source /opt/Algoryx/AGX-2.38.0.0/setup_env.bash
+export LD_LIBRARY_PATH=/opt/Algoryx/AGX-2.38.0.0/lib:
+source /opt/ros/humble/setup.bash
+source /home/ubuntu/ros2_ws/install/setup.bash
 ros2 launch WRS2024pre_ROS2 WRS2024pre_preliminaries.launch.xml
