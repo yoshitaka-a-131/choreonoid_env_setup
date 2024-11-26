ef38512ea1ba4c45a2167d55b3adeaef79d63c79
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2024-11-27 00:34:30 +0900

    Update WRS2024pre_preliminaries.sh

--- desktop/WRS2024pre_preliminaries.sh
+++ desktop/WRS2024pre_preliminaries.sh
@@ -3,4 +3,4 @@ source /opt/Algoryx/AGX-2.38.0.0/setup_env.bash
 export LD_LIBRARY_PATH=/opt/Algoryx/AGX-2.38.0.0/lib:
 source /opt/ros/humble/setup.bash
 source /home/ubuntu/ros2_ws/install/setup.bash
-ros2 launch choreonoid_ros2_sample_wrs2024pre WRS2024pre_preliminaries.launch.xml
+ros2 launch choreonoid_ros2_wrs2024pre_sample WRS2024pre_preliminaries.launch.xml
